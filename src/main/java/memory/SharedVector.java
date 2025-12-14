3ada5da1b2c469722a11fe2958aa4106e6d41054
Z-I-V <ziv8800@gmail.com>
2025-12-14 23:51:12 +0200

    Shared Matrix except the vecMatMul function

--- src/main/java/memory/SharedVector.java
+++ src/main/java/memory/SharedVector.java
@@ -1,6 +1,7 @@
 package memory;
 
 import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock.ReadLock;
 
 public class SharedVector {
 
@@ -10,54 +11,124 @@ public class SharedVector {
 
     public SharedVector(double[] vector, VectorOrientation orientation) {
         // TODO: store vector data and its orientation
+        this.vector = vector;
+        this.orientation = orientation;
     }
 
     public double get(int index) {
         // TODO: return element at index (read-locked)
-        return 0;
+        lock.readLock().lock();
+        try {
+            return vector[index];
+        } finally {
+            lock.readLock().unlock();
+        }  
     }
 
     public int length() {
         // TODO: return vector length
-        return 0;
+        lock.readLock().lock();
+        try {   
+            return vector.length;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public VectorOrientation getOrientation() {
         // TODO: return vector orientation
-        return null;
+        lock.readLock().lock();
+        try {   
+            return orientation;
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void writeLock() {
         // TODO: acquire write lock
+        lock.writeLock().lock();
     }
 
     public void writeUnlock() {
         // TODO: release write lock
+        lock.writeLock().unlock();
     }
 
     public void readLock() {
         // TODO: acquire read lock
+        lock.readLock().lock();
     }
 
     public void readUnlock() {
         // TODO: release read lock
+        lock.readLock().unlock();
     }
 
     public void transpose() {
         // TODO: transpose vector
+        lock.writeLock().lock();
+        try {
+            if (orientation == VectorOrientation.ROW_MAJOR) {
+                orientation = VectorOrientation.COLUMN_MAJOR;
+            } else {
+                orientation = VectorOrientation.ROW_MAJOR;
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
-
     public void add(SharedVector other) {
         // TODO: add two vectors
+        lock.writeLock().lock();
+        other.readLock();
+        try {
+            if (other.length() != this.length()) {
+                throw new IllegalArgumentException("Vectors must be of the same length to add.");
+            }
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] += other.get(i);
+            }
+        } finally {
+            other.readUnlock();
+            lock.writeLock().unlock();
+        }
     }
 
     public void negate() {
         // TODO: negate vector
+        lock.writeLock().lock();
+        try {
+            for (int i = 0; i < vector.length; i++) {
+                vector[i] = -vector[i];
+            }
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public double dot(SharedVector other) {
         // TODO: compute dot product (row · column)
-        return 0;
+        lock.readLock().lock();
+        other.readLock();  
+        try {
+            if (other.length() != this.length()) {
+                throw new IllegalArgumentException("Vectors must be of the same length to compute dot product.");
+            }
+            if (this.orientation != other.getOrientation()) {
+                throw new IllegalArgumentException(
+                    "Vectors must have the same orientation to add. " +
+                    "This: " + this.orientation + ", Other: " + other.getOrientation());
+            }
+            double result = 0.0;
+            for (int i = 0; i < vector.length; i++) {
+                result += this.vector[i] * other.get(i);
+            }
+            return result;
+        } finally {
+            other.readUnlock();
+            lock.readLock().unlock();
+        }
     }
 
     public void vecMatMul(SharedMatrix matrix) {
